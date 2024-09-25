75902de1cd87e9e2edc4c236b0e7779877c7889b
Kristoffer Lerbæk Pedersen <kristp@danskebank.dk>
2024-09-25 14:46:09 +0200

    Add integration test project that will fail at first

---
+++ IntegrationTests/Configuration/Factories/IntegrationTestWebAppFactory.cs
@@ -0,0 +1,60 @@
+﻿using DotNet.Testcontainers.Configurations;
+using DotNet.Testcontainers.Images;
+using IntegrationTests.Configuration.Fixtures;
+using MassTransit;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+
+namespace IntegrationTests.Configuration.Factories;
+
+public abstract class IntegrationTestWebAppFactory<TEntryPoint> : WebApplicationFactory<TEntryPoint>, IAsyncLifetime
+    where TEntryPoint : class
+{
+    private readonly RabbitMqFixture _rabbitMqFixture;
+
+    protected IntegrationTestWebAppFactory()
+    {
+        TestcontainersSettings.ResourceReaperImage = new DockerImage("remote-docker-hub.artifactory.danskenet.net/testcontainers/ryuk:0.5.1");
+        _rabbitMqFixture = new RabbitMqFixture();
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            services.AddMassTransitTestHarness();
+        });
+
+        var inMemoryConfig = new Dictionary<string, string?>
+        {
+            ["RabbitMq:Host"] = "127.0.0.1",
+            ["RabbitMq:Port"] = _rabbitMqFixture.Port.ToString(),
+            ["RabbitMq:Retries"] = "2",
+        };
+
+        var configurationBuilder = new ConfigurationBuilder();
+        configurationBuilder.AddInMemoryCollection(inMemoryConfig);
+        var configuration = configurationBuilder.Build();
+
+        builder.UseConfiguration(configuration)
+            .ConfigureAppConfiguration((_, config) =>
+            {
+                config.AddInMemoryCollection(inMemoryConfig);
+            });
+    }
+
+
+    public async Task InitializeAsync()
+    {
+        await _rabbitMqFixture.InitializeAsync();
+    }
+
+    public new async Task DisposeAsync()
+    {
+        await _rabbitMqFixture.DisposeAsync();
+        await base.DisposeAsync();
+    }
+}
---
+++ IntegrationTests/Configuration/Fixtures/RabbitMqFixture.cs
@@ -0,0 +1,24 @@
+﻿using Testcontainers.RabbitMq;
+using Xunit;
+
+namespace IntegrationTests.Configuration.Fixtures;
+
+public sealed class RabbitMqFixture : IAsyncLifetime
+{
+    private readonly RabbitMqContainer _rabbitMqContainer = new RabbitMqBuilder()
+        .WithName($"testcontainers-rabbitmq-{Guid.NewGuid()}")
+        .WithImage("remote-docker-hub.artifactory.danskenet.net/rabbitmq:3.11.20-management")
+        .Build();
+
+    public async Task InitializeAsync()
+    {
+        await _rabbitMqContainer.StartAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _rabbitMqContainer.StopAsync();
+    }
+
+    public int Port => _rabbitMqContainer.GetMappedPublicPort(5672);
+}
